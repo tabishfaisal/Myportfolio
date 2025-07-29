03962368cae82d83a4b5a9b53e2fdaf050cdd3ba
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2025-07-29 15:42:51 +0200

    edited about page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,25 @@
 import './Aboutstyle.css';
+import profile from '../../assets/profile.png';
 
 const About = () => {
   return (
     <div className="about">
-      <h1>About</h1>
+      <img className='about-img' src={profile} alt="My profile Picture" />
+      <div className="about-2">
+        <h1 className='about-name'>I am Tabish Faisal</h1>
+        <p>
+          I have a strong technical background with a Diploma in Computer Science and have completed
+          a Full Stack Web Developer bootcamp. My experience spans frontend and backend development,
+          with a focus on modern technologies like React, JavaScript, Node.js, and Express.
+        </p>
+        <p>
+          I am passionate about building efficient, scalable web applications and continuously improving
+          my skills to create innovative digital solutions.
+        </p>
+
+      </div>
     </div>
+
   )
 }
 
